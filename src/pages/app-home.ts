025c13df9c99979290636bfb71aeab0a7ae0868c
miss-mercurial <sigvardt89@gmail.com>
2023-10-23 21:04:10 +0200

    Added newlines and changed welcometext.

--- src/pages/app-home.ts
+++ src/pages/app-home.ts
@@ -12,7 +12,7 @@ export class AppHome extends LitElement {
 
   // For more information on using properties and state in lit
   // check out this link https://lit.dev/docs/components/properties/
-  @property() message = 'Welcome!';
+  @property() message = 'Hello World!🦄';
 
   static styles = [
     styles,
