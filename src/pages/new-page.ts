3513cb10d9446db7c360f2c9d20bd4c69c20dfd8
miss-mercurial <sigvardt89@gmail.com>
2023-11-27 16:38:27 +0100

    Added sl-card to contain all the elements. Deleted old test-card and test-dropdown.

--- src/pages/new-page.ts
+++ src/pages/new-page.ts
@@ -18,38 +18,17 @@ export class AppSettings extends LitElement {
     render() {
         return html`
             <app-header ?enableBack="${true}"></app-header>
-
             <main>
-            <sl-input label="What is your dilemma?" autocomplete="off"></sl-input>
+                <sl-card class="card-basic">
+                    <sl-input label="The dilemma?" autocomplete="off" autofocus="on"></sl-input>
 
-            <sl-card>
-                <h2>Woop woop</h2>
-
-                <p>
-                    It worked 😎
-                </p>
-
-                <p>
-                    🐱‍💻
-                </p>
-            </sl-card>
-
-            <sl-dropdown>
-                <sl-button slot="trigger" caret>Dropdown</sl-button>
-                <sl-menu>
-                    <sl-menu-item>Dropdown Item 1</sl-menu-item>
-                    <sl-menu-item>Dropdown Item 2</sl-menu-item>
-                    <sl-menu-item>Dropdown Item 3</sl-menu-item>
-                </sl-menu>
-            </sl-dropdown>
-
-            <div class="custom-selector scheme-pro">
-                <importance-selector></importance-selector>
-            </div>
-            <div class="custom-selector scheme-con">
-                <importance-selector></importance-selector>
-            </div>
-
+                    <div class="custom-selector scheme-pro">
+                        <importance-selector></importance-selector>
+                    </div>
+                    <div class="custom-selector scheme-con">
+                        <importance-selector></importance-selector>
+                    </div>
+                </sl-card>
             </main>
         `;
     }
