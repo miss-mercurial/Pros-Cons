133adcef62b52bf5b94663cdcf16505fd710cbc3
miss-mercurial <sigvardt89@gmail.com>
2023-11-27 16:53:39 +0100

    Added sensitivity selector and headers.

--- src/pages/new-page.ts
+++ src/pages/new-page.ts
@@ -2,9 +2,13 @@ import { LitElement, html} from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 import '@shoelace-style/shoelace/dist/components/card/card';
-import '@shoelace-style/shoelace/dist/components/dropdown/dropdown';
 import '@shoelace-style/shoelace/dist/components/menu/menu';
 import '@shoelace-style/shoelace/dist/components/menu-item/menu-item';
+
+import '@shoelace-style/shoelace/dist/components/input/input';
+import '@shoelace-style/shoelace/dist/components/select/select';
+import '@shoelace-style/shoelace/dist/components/option/option';
+
 
 import { styles as sharedStyles } from '../styles/shared-styles'
 import '../components/importance/importance'
@@ -21,7 +25,12 @@ export class AppSettings extends LitElement {
             <main>
                 <sl-card class="card-basic">
                     <sl-input label="The dilemma?" autocomplete="off" autofocus="on"></sl-input>
-
+                    <sl-select label="How sure would you like to be?">
+                        <sl-option value="option-1">Option 1</sl-option>
+                        <sl-option value="option-2">Option 2</sl-option>
+                        <sl-option value="option-3">Option 3</sl-option>
+                    </sl-select>
+                    <p>Importance --- Matter</p>
                     <div class="custom-selector scheme-pro">
                         <importance-selector></importance-selector>
                     </div>
