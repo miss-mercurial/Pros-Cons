0d607eec0728aad4b5f6012f1021790b8ccbf8b9
miss-mercurial <sigvardt89@gmail.com>
2023-11-28 13:23:59 +0100

    Created background-card component with a <slot> for projection

---
+++ src/components/background-card/background-card.ts
@@ -0,0 +1,26 @@
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import '@shoelace-style/shoelace/dist/components/card/card';
+
+import { styles as sharedStyles } from '../../styles/shared-styles';
+import { styles } from './background-card-styles';
+
+@customElement('background-card')
+export class BackgroundCard extends LitElement {
+    static styles = [
+        sharedStyles,
+        styles
+    ];
+
+    render() {
+        return html`
+            <div class="center-container">
+                <sl-card class="card-basic">
+                    <!-- Your card content goes here -->
+                    <slot></slot>
+                </sl-card>
+            </div>
+        `;
+    }
+}
