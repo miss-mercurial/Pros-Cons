70d3cca22189606c571a36457ee85be52df5fb03
miss-mercurial <sigvardt89@gmail.com>
2023-11-28 13:24:18 +0100

    Updated with background-card component

--- src/pages/new-page.ts
+++ src/pages/new-page.ts
@@ -11,6 +11,7 @@ import '@shoelace-style/shoelace/dist/components/option/option';
 
 import { styles as sharedStyles } from '../styles/shared-styles';
 import '../components/importance/importance'
+import '../components/background-card/background-card'
 
 @customElement('new-page')
 export class AppSettings extends LitElement {
@@ -22,27 +23,29 @@ export class AppSettings extends LitElement {
         return html`
             <app-header ?enableBack="${true}"></app-header>
             <main>
-                <sl-card class="card-basic">
-                    <sl-input label="The dilemma?" autocomplete="off" autofocus></sl-input>
-                    <sl-select label="How sure would you like to be?">
-                        <sl-option value="option-1">Option 1</sl-option>
-                        <sl-option value="option-2">Option 2</sl-option>
-                        <sl-option value="option-3">Option 3</sl-option>
-                    </sl-select>
-                    <p>Importance -------------- Matter</p>
-                    <div class="custom-selector scheme-pro">
-                        <importance-selector></importance-selector>
-                    </div>
-                    <div class="custom-selector scheme-con">
-                        <importance-selector></importance-selector>
-                    </div>
-                    <p>Conclusion</p>
-                    <div style="text-align: center;">
-                        <p>??% pros</p>
-                        <p>??% cons</p>
-                        <h4>Do it 👍|Think more about it 🤔|Don't do it 👎</h4>
-                    </div>
-                </sl-card>
+                <div class="center-container">
+                    <background-card>
+                        <sl-input label="The dilemma?" autocomplete="off" autofocus></sl-input>
+                        <sl-select label="How sure would you like to be?">
+                            <sl-option value="option-1">Option 1</sl-option>
+                            <sl-option value="option-2">Option 2</sl-option>
+                            <sl-option value="option-3">Option 3</sl-option>
+                        </sl-select>
+                        <p>Importance -------------- Matter</p>
+                        <div class="custom-selector scheme-pro">
+                            <importance-selector></importance-selector>
+                        </div>
+                        <div class="custom-selector scheme-con">
+                            <importance-selector></importance-selector>
+                        </div>
+                        <p>Conclusion</p>
+                        <div style="text-align: center;">
+                            <p>??% pros</p>
+                            <p>??% cons</p>
+                            <h4>Do it 👍|Think more about it 🤔|Don't do it 👎</h4>
+                        </div>
+                    </background-card>
+                </div>
             </main>
         `;
     }
