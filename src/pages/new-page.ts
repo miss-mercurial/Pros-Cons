1c088bc65a1f72fa132b9e8430161ef250175162
miss-mercurial <sigvardt89@gmail.com>
2023-11-28 12:15:03 +0100

    semicolon and mockup

--- src/pages/new-page.ts
+++ src/pages/new-page.ts
@@ -9,8 +9,7 @@ import '@shoelace-style/shoelace/dist/components/input/input';
 import '@shoelace-style/shoelace/dist/components/select/select';
 import '@shoelace-style/shoelace/dist/components/option/option';
 
-
-import { styles as sharedStyles } from '../styles/shared-styles'
+import { styles as sharedStyles } from '../styles/shared-styles';
 import '../components/importance/importance'
 
 @customElement('new-page')
@@ -24,18 +23,24 @@ export class AppSettings extends LitElement {
             <app-header ?enableBack="${true}"></app-header>
             <main>
                 <sl-card class="card-basic">
-                    <sl-input label="The dilemma?" autocomplete="off" autofocus="on"></sl-input>
+                    <sl-input label="The dilemma?" autocomplete="off" autofocus></sl-input>
                     <sl-select label="How sure would you like to be?">
                         <sl-option value="option-1">Option 1</sl-option>
                         <sl-option value="option-2">Option 2</sl-option>
                         <sl-option value="option-3">Option 3</sl-option>
                     </sl-select>
-                    <p>Importance --- Matter</p>
+                    <p>Importance -------------- Matter</p>
                     <div class="custom-selector scheme-pro">
                         <importance-selector></importance-selector>
                     </div>
                     <div class="custom-selector scheme-con">
                         <importance-selector></importance-selector>
+                    </div>
+                    <p>Conclusion</p>
+                    <div style="text-align: center;">
+                        <p>??% pros</p>
+                        <p>??% cons</p>
+                        <h4>Do it 👍|Think more about it 🤔|Don't do it 👎</h4>
                     </div>
                 </sl-card>
             </main>
