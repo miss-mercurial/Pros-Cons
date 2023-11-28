709da9398bef8d1aca0d1d7864f3349f4cc6a3e3
miss-mercurial <sigvardt89@gmail.com>
2023-11-28 13:23:23 +0100

    Created style for background-card component

---
+++ src/components/background-card/background-card-styles.ts
@@ -0,0 +1,9 @@
+import { css } from "lit";
+
+export const styles = css`
+    .center-container {
+        display: flex;
+        justify-content: center;
+        /*height: 100vh;  Adjust the height as needed */
+    }
+`;
