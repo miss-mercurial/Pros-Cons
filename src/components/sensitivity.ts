974c66393a5d5c61ddd25f9ecabaa2593d35d781
miss-mercurial <sigvardt89@gmail.com>
2023-12-03 21:05:00 +0100

    Added "%" suffix to input

--- src/components/sensitivity.ts
+++ src/components/sensitivity.ts
@@ -1,9 +1,8 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
-import '@shoelace-style/shoelace/dist/components/select/select';
-import '@shoelace-style/shoelace/dist/components/option/option';
 import '@shoelace-style/shoelace/dist/components/input/input'
+
 
 import { styles as sharedStyles } from '../styles/shared-styles';
 
@@ -15,8 +14,17 @@ export class SensitivitySelector extends LitElement {
 
     render() {
         return html`
-            <label>How sure would you like to be?</label>
-            <sl-input style="text-align: right;" type="number" id="quantity" name="quantity" min="0" max="100" step="1" value="60"></sl-input>
+            <sl-input
+                label="How sure would you like to be?"
+                type="number"
+                id="quantity"
+                name="quantity"
+                min="0"
+                max="100"
+                step="1"
+                value="60">
+                <span slot="suffix">%</span>
+            </sl-input>
         `;
     }
 }
