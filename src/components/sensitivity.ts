ab4eed4cde276d7cef78b57aec242c965547feb8
miss-mercurial <sigvardt89@gmail.com>
2023-12-04 23:33:58 +0100

    Input check corrected

--- src/components/sensitivity.ts
+++ src/components/sensitivity.ts
@@ -5,12 +5,36 @@ import '@shoelace-style/shoelace/dist/components/input/input'
 
 
 import { styles as sharedStyles } from '../styles/shared-styles';
+import { SlInputEvent } from '@shoelace-style/shoelace';
 
 @customElement('sensitivity-selector')
 export class SensitivitySelector extends LitElement {
     static styles = [
         sharedStyles
     ];
+
+    firstUpdated() {
+        const input = this.shadowRoot?.querySelector('sl-input');
+
+        if (input) {
+            input.addEventListener('sl-change', (event: SlInputEvent) => {
+                this.handleInputChange(event.target as HTMLInputElement);
+            });
+        }
+    }
+
+    /**
+     * Validate and correct the input value
+     *
+     * @param inputElement The InputElement the value of wich to do the validation on.
+     */
+    private handleInputChange(inputElement: HTMLInputElement) {
+        const value: number = inputElement.valueAsNumber;
+        if (value > 100)
+            inputElement.value = "100";
+        else if (value < 0)
+            inputElement.value = "0";
+    }
 
     render() {
         return html`
@@ -22,7 +46,8 @@ export class SensitivitySelector extends LitElement {
                 min="0"
                 max="100"
                 step="1"
-                value="60">
+                value="50">
+            >
                 <span slot="suffix">%</span>
             </sl-input>
         `;
