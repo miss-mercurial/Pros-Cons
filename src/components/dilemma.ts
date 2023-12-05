f4a7c4802809c09821c5f9789077d398167596a3
miss-mercurial <sigvardt89@gmail.com>
2023-12-05 15:09:41 +0100

    Created dilemma component - does not work

---
+++ src/components/dilemma.ts
@@ -0,0 +1,26 @@
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import '@shoelace-style/shoelace/dist/components/input/input'
+
+import { styles as sharedStyles } from '../styles/shared-styles';
+
+@customElement('dilemma-input')
+export class DilemmaInput extends LitElement {
+    static styles = [
+        sharedStyles
+    ];
+
+    render() {
+        return html`
+            <sl-input style="border: 2px solid blue;" label="The dilemma?"></sl-input>
+        `;
+    }
+}
+/* <sl-input style="border: 2px solid blue;"
+                label="The dilemma?"
+                autocomplete="off"
+                autofocus
+            >
+                <span slot="suffix">🤔</span>
+            </sl-input> */
