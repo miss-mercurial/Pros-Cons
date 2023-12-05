8bb5821390877761eac141373c6019080caa2cbe
miss-mercurial <sigvardt89@gmail.com>
2023-12-05 14:43:46 +0100

    Removed import select

--- src/pages/new-page.ts
+++ src/pages/new-page.ts
@@ -6,7 +6,6 @@ import '@shoelace-style/shoelace/dist/components/menu/menu';
 import '@shoelace-style/shoelace/dist/components/menu-item/menu-item';
 
 import '@shoelace-style/shoelace/dist/components/input/input';
-import '@shoelace-style/shoelace/dist/components/select/select';
 import '@shoelace-style/shoelace/dist/components/option/option';
 
 import { styles as sharedStyles } from '../styles/shared-styles';
